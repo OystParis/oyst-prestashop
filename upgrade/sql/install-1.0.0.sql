cb23b290d52f0312929f83248cf62f167dfbbb38
DECRAENE Nelly <nelly@oyst.com>
2017-03-13 18:40:11 +0100

    Update URL endpoint

---
+++ upgrade/sql/install-1.0.0.sql
@@ -0,0 +1,2 @@
+UPDATE `@PREFIX@configuration` SET `value` = 'https://api.oyst.com/payment' WHERE `name` = 'FC_OYST_API_PAYMENT_ENDPOINT';
+UPDATE `@PREFIX@configuration` SET `value` = 'https://api.oyst.com/catalog' WHERE `name` = 'FC_OYST_API_CATALOG_ENDPOINT';
