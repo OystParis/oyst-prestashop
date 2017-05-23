7c2ae0d36f8772a2f84483afc6fe520a178a0d30
DECRAENE Nelly <nelly@oyst.com>
2017-05-23 08:17:04 +0000

    ZH110 - Fix partial refund coming from FreePay BO (allowed several times)

--- controllers/front/paymentNotification.php
+++ controllers/front/paymentNotification.php
@@ -70,7 +70,7 @@ class OystPaymentNotificationModuleFrontController extends ModuleFrontController
                     // If refund is confirmed, we cancel the order
                     case OystPaymentNotification::EVENT_REFUND:
                         $oystOrderRepository = new OrderRepository(Db::getInstance());
-                        $maxRefund = $oystOrderRepository->getOrderMaxRefund($id_cart);
+                        $maxRefund = $oystOrderRepository->calculateOrderMaxRefund($id_cart);
                         $status = $maxRefund == 0 ? Configuration::get('PS_OS_REFUND') : Configuration::get('OYST_STATUS_PARTIAL_REFUND');
 
                         $this->updateOrderStatus((int)$id_cart, $status);
