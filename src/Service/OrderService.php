187d75b3436942b5d9347739f48e3c2af95a6e6f
Romain FEY <romain.fey@pilipili-web.com>
2017-10-12 16:17:45 +0200

    Hotfix/ Calcul shipping and code iso relay

--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -128,7 +128,7 @@ class OrderService extends AbstractOystService
 
         $address = $this->addressRepository->findAddress($addressToFind);
         if (!Validate::isLoadedObject($address)) {
-            $countryId = (int)CountryCore::getByIso($addressToFind['country']);
+            $countryId = (int)CountryCore::getByIso('fr');
             if (0 >= $countryId) {
                 $countryId = PSConfiguration::get('PS_COUNTRY_DEFAULT');
             }
