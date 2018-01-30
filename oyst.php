dfacdccf3cd032793fae60e1e12cfc9e8e1f9166
Romain FEY <romain.fey@pilipili-web.com>
2018-01-30 11:03:37 +0100

    Update version

--- oyst.php
+++ oyst.php
@@ -32,7 +32,7 @@ class Oyst extends FroggyPaymentModule
     public function __construct()
     {
         $this->name = 'oyst';
-        $this->version = '1.7.1';
+        $this->version = '1.8.0';
         $this->tab = 'payments_gateways';
 
         parent::__construct();
