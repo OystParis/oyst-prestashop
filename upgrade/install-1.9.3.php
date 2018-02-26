2e9979040fe244f37022980e89c2769a76c35ae7
Kévin S <kevin.sonnet@bwagence.fr>
2018-02-26 19:13:34 +0100

    Add upgrade file

---
+++ upgrade/install-1.9.3.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * 2013-2016 Froggy Commerce
+ *
+ * NOTICE OF LICENSE
+ *
+ * You should have received a licence with this module.
+ * If you didn't download this module on Froggy-Commerce.com, ThemeForest.net,
+ * Addons.PrestaShop.com, or Oyst.com, please contact us immediately : contact@froggy-commerce.com
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to benefit the updates
+ * for newer PrestaShop versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Froggy Commerce <contact@froggy-commerce.com>
+ * @copyright 2013-2016 Froggy Commerce / 23Prod / Oyst
+ * @license   GNU GENERAL PUBLIC LICENSE
+ */
+
+require_once dirname(__FILE__).'/../vendor/autoload.php';
+
+/**
+ * @param Oyst $module
+ * @return bool
+ */
+function upgrade_module_1_9_3($module)
+{
+    Db::getInstance()->execute("ALTER TABLE "._DB_PREFIX_."oyst_payment_notification ADD `status` varchar(255) DEFAULT NULL");
+    Db::getInstance()->execute("ALTER TABLE "._DB_PREFIX_."oyst_payment_notification ADD `date_upd` datetime DEFAULT NULL");
+
+    return true;
+}
