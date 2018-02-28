47a2a986cce03b3a115b83ef35eca8291ea39504
Romain FEY <romain.fey@pilipili-web.com>
2018-02-28 17:51:13 +0100

    Fix when user is not log

--- controllers/front/oneclickreturn.php
+++ controllers/front/oneclickreturn.php
@@ -47,6 +47,20 @@ class OystOneclickreturnModuleFrontController extends ModuleFrontController
             die('Wrong security key');
         }
 
+        // Log user
+        $this->context->cookie->id_compare = isset($this->context->cookie->id_compare) ? $this->context->cookie->id_compare: CompareProduct::getIdCompareByIdCustomer($customer->id);
+        $this->context->cookie->id_customer = (int)($customer->id);
+        $this->context->cookie->customer_lastname = $customer->lastname;
+        $this->context->cookie->customer_firstname = $customer->firstname;
+        $this->context->cookie->logged = 1;
+        $customer->logged = 1;
+        $this->context->cookie->is_guest = $customer->isGuest();
+        $this->context->cookie->passwd = $customer->passwd;
+        $this->context->cookie->email = $customer->email;
+        // Add customer to the context
+        $this->context->customer = $customer;
+        $this->context->cookie->write();
+
         // Load cart and order
         $id_order = Order::getOrderByCartId($id_cart);
         $order = new Order($id_order);
