0018f77586b88630317e820ab4f84b8b9355b208
Romain FEY <romain.fey@pilipili-web.com>
2018-03-01 16:52:45 +0100

    Update cart.estimate

--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -230,9 +230,9 @@ class CartService extends AbstractOystService
                 }
 
                 $oneClickItem = new OneClickItem((string)$item['product']['reference'], (int)$item['product']['quantity']);
-                $amount = new OystPrice(round($price * 100), Context::getContext()->currency->iso_code);
+                $amount = new OystPrice($price, Context::getContext()->currency->iso_code);
                 $oneClickItem->setAmountOriginal($amount);
-                $crossed_out_amount = new OystPrice(round($without_reduc_price * 100), Context::getContext()->currency->iso_code);
+                $crossed_out_amount = new OystPrice($without_reduc_price, Context::getContext()->currency->iso_code);
                 $oneClickItem->setAmountPromotional($crossed_out_amount);
 
                 $oneClickShipmentCalculation->addItem($oneClickItem);
