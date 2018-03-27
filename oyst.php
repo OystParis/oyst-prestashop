a75b086e8fd83abbc5fe94d4ffbdbda48448fb43
Kévin Sonnet <kevin.sonnet@bwagence.fr>
2018-03-27 09:56:05 +0200

    Admin synthax error fix (#573)

--- oyst.php
+++ oyst.php
@@ -32,7 +32,7 @@ class Oyst extends FroggyPaymentModule
     public function __construct()
     {
         $this->name = 'oyst';
-        $this->version = '1.14.0';
+        $this->version = '1.14.1';
         $this->tab = 'payments_gateways';
 
         parent::__construct();
