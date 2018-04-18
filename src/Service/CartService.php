d7aa228d2b4e8e020be84b3ee8a6165ef84d3f29
Romain FEY <romain.fey@pilipili-web.com>
2018-04-18 11:42:40 +0200

    Add order_amount

--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -588,6 +588,13 @@ class CartService extends AbstractOystService
             }
         }
 
+        $cart_amount = $cart->getOrderTotal(true);
+
+        if ($cart_amount > 0) {
+            $cart_amount_oyst = new OystPrice($cart->getOrderTotal(true), Context::getContext()->currency->iso_code);
+            $oneClickOrderCartEstimate->setOrderAmount($cart_amount_oyst);
+        }
+
         $this->logger->info(
             sprintf(
                 'New notification oneClickOrderCartEstimate [%s]',
