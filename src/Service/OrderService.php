06196fce87f7d29cf9a02e030ef47db3035cd670
Romain FEY <romain.fey@pilipili-web.com>
2018-05-11 11:15:04 +0200

    Add module colissimo simplicite

--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -443,8 +443,11 @@ class OrderService extends AbstractOystService
             }
 
             if (($carrierInfo['type'] == 'colissimo_poste' || $carrierInfo['type'] == 'colissimo_commerces') &&
-                Module::isEnabled('soflexibilite') &&
-                Module::isInstalled('soflexibilite')) {
+                (Module::isEnabled('soflexibilite') &&
+                Module::isInstalled('soflexibilite')) ||
+                (Module::isEnabled('socolissimo') &&
+                Module::isInstalled('socolissimo'))
+            ) {
                 if ($carrierInfo['type'] == 'colissimo_poste') {
                     $delivery_mode = 'BPR';
                 } else {
