2eba4c5f1daea5b86a1ebe0517dbdec612ba7fe9
Romain FEY <romain.fey@pilipili-web.com>
2018-05-11 13:36:46 +0200

    Manage button layer, login, process order, opc, form address

--- upgrade/install-1.15.0.php
+++ upgrade/install-1.15.0.php
@@ -21,14 +21,62 @@
 
 require_once dirname(__FILE__).'/../vendor/autoload.php';
 
-use Oyst\Service\Configuration as OConfiguration;
 
 /**
  * @param Oyst $module
  * @return bool
  */
+
 function upgrade_module_1_15_0($module)
 {
     $module->registerHook('actionValidateOrder');
+
+    $module->unregisterHook('displayFooterProduct');
+    $module->unregisterHook('displayShoppingCart');
+    $module->registerHook('displayFooter');
+
+    // Remove old configuration button product
+    Configuration::deleteByName('FC_OYST_BTN_PRODUCT');
+    Configuration::deleteByName('FC_OYST_WIDTH_BTN');
+    Configuration::deleteByName('FC_OYST_HEIGHT_BTN');
+    Configuration::deleteByName('FC_OYST_MARGIN_TOP_BTN');
+    Configuration::deleteByName('FC_OYST_MARGIN_LEFT_BTN');
+    Configuration::deleteByName('FC_OYST_MARGIN_RIGHT_BTN');
+    Configuration::deleteByName('FC_OYST_POSITION_BTN');
+    Configuration::deleteByName('FC_OYST_ID_BTN_ADD_TO_CART');
+    Configuration::deleteByName('FC_OYST_ID_SMART_BTN');
+
+    // Rename configuration button product
+    Configuration::updateValue('FC_OYST_BTN_PRODUCT', 1);
+    Configuration::updateValue('FC_OYST_WIDTH_BTN_PRODUCT');
+    Configuration::updateValue('FC_OYST_HEIGHT_BTN_PRODUCT');
+    Configuration::updateValue('FC_OYST_MARGIN_TOP_BTN_PRODUCT');
+    Configuration::updateValue('FC_OYST_MARGIN_LEFT_BTN_PRODUCT');
+    Configuration::updateValue('FC_OYST_MARGIN_RIGHT_BTN_PRODUCT');
+    Configuration::updateValue('FC_OYST_POSITION_BTN_PRODUCT', 'before');
+    Configuration::updateValue('FC_OYST_ID_BTN__PRODUCT', '#add_to_cart');
+    Configuration::updateValue('FC_OYST_ID_SMART_BTN_PRODUCT', '#add_to_cart button');
+
+    // Add conf custom btn layer
+    Configuration::updateValue('FC_OYST_BTN_LAYER', 0);
+    Configuration::updateValue('FC_OYST_WIDTH_BTN_LAYER', '214');
+    Configuration::updateValue('FC_OYST_HEIGHT_BTN_LAYER', '43');
+    Configuration::updateValue('FC_OYST_MARGIN_TOP_BTN_LAYER', '');
+    Configuration::updateValue('FC_OYST_MARGIN_LEFT_BTN_LAYER', '');
+    Configuration::updateValue('FC_OYST_MARGIN_RIGHT_BTN_LAYER', '');
+    Configuration::updateValue('FC_OYST_ID_BTN_LAYER', '#layer_cart .button-container');
+    Configuration::updateValue('FC_OYST_POSITION_BTN_LAYER', 'before');
+
+    // Add conf custom btn login
+    Configuration::updateValue('FC_OYST_BTN_LOGIN', 0);
+    Configuration::updateValue('FC_OYST_WIDTH_BTN_LOGIN', '');
+    Configuration::updateValue('FC_OYST_HEIGHT_BTN_LOGIN', '');
+    Configuration::updateValue('FC_OYST_MARGIN_TOP_BTN_LOGIN', '');
+    Configuration::updateValue('FC_OYST_MARGIN_LEFT_BTN_LOGIN', '');
+    Configuration::updateValue('FC_OYST_MARGIN_RIGHT_BTN_LOGIN', '');
+    Configuration::updateValue('FC_OYST_ID_BTN_LOGIN', '#SubmitCreate');
+    Configuration::updateValue('FC_OYST_ID_SMART_BTN_LOGIN', '#SubmitCreate');
+    Configuration::updateValue('FC_OYST_POSITION_BTN_LOGIN', 'before');
+
     return true;
 }
