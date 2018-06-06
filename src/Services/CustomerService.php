bc8d71335e9940f7e24626f19c92c01b3767882e
Kévin S <kevin.sonnet@bwagence.fr>
2018-06-06 15:32:36 +0200

    Remove search by phone

--- src/Services/CustomerService.php
+++ src/Services/CustomerService.php
@@ -3,7 +3,6 @@
 namespace Oyst\Services;
 
 use Customer;
-use Db;
 use Language;
 use Validate;
 
@@ -38,20 +37,6 @@ class CustomerService {
         if (empty($addresses) && !empty($customer_infos['email'])) {
             $customer = new Customer();
             $customer->getByEmail($customer_infos['email']);
-            if (Validate::isLoadedObject($customer)) {
-                $addresses = $customer->getAddresses($id_lang_fr);
-            }
-        }
-
-        //Search on phone number
-        if (empty($addresses) && !empty($customer_infos['phone'])) {
-            $id_customer = Db::getInstance()->getValue("SELECT `c`.`id_customer` 
-                FROM `"._DB_PREFIX_."customer` `c`
-                INNER JOIN `"._DB_PREFIX_."address` `a` ON `c`.`id_customer` = `a`.`id_customer`
-                WHERE `a`.`phone` LIKE '".pSQL($customer_infos['phone'])."' OR `a`.`phone_mobile` LIKE '".pSQL($customer_infos['phone'])."'
-                ORDER BY `c`.`date_upd` DESC");
-
-            $customer = new Customer($id_customer);
             if (Validate::isLoadedObject($customer)) {
                 $addresses = $customer->getAddresses($id_lang_fr);
             }
