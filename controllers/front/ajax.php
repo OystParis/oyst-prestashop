2e4582d97e603bd0775ff3810aacb640a18fd862
Kévin S <kevin.sonnet@bwagence.fr>
2018-06-07 18:38:44 +0200

    Fix ajax add to cart url + move ajax.php to controller

---
+++ controllers/front/ajax.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class OystAjaxModuleFrontController extends ModuleFrontController
+{
+    public function displayAjax()
+    {
+        header('Content-Type: application/json');
+
+        if (empty($_GET['action'])) {
+            die(json_encode(array('error' => 'Action undefined')));
+        }
+
+        switch ($_GET['action']) {
+            case 'get_id_cart':
+                $id_cart = Context::getContext()->cart->id;
+                die(json_encode(array('id_cart' => $id_cart)));
+                break;
+            default:
+                die(json_encode(array('error' => 'Unknown action')));
+        }
+    }
+}
