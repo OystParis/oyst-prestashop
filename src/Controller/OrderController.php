5c52c0d7d5a816d3859c9fb33173cbb41a7b1ef4
Kévin S <kevin.sonnet@bwagence.fr>
2018-06-11 17:45:42 +0200

    Add refund full order

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,6 +3,7 @@
 namespace Oyst\Controller;
 
 use Order;
+use OrderSlip;
 use Oyst\Classes\Notification;
 use Oyst\Services\OrderService;
 use Validate;
@@ -37,14 +38,30 @@ class OrderController extends AbstractOystController
             $id_order = Notification::getOrderIdByOystOrderId($params['url']['id']);
             $order = new Order($id_order);
             if (Validate::isLoadedObject($order)) {
+                $result = array();
                 if (!empty($params['data']['id_order_state'])) {
                     if ($order->current_state != $params['data']['id_order_state']) {
                         $order->setCurrentState($params['data']['id_order_state']);
-                        $this->respondAsJson(array('success' => true));
+                        $result['change_order_state'] = array('success' => true);
                     } else {
-                        $this->respondAsJson(array('error' => 'The order already has this status'));
+                        $result['change_order_state'] = array('error' => 'The order already has this status');
                     }
                 }
+
+                if (!empty($params['data']['refund'])) {
+                    $amount = 0;
+                    $amount_choosen = false;
+                    $products_list = array();
+                    $shipping_cost = $order->total_shipping_tax_excl;
+
+                    if (OrderSlip::create($order, $products_list, $shipping_cost, $amount, $amount_choosen)) {
+                        $result['refund'] = 'Order slip created successfully';
+                    } else {
+                        $result['refund'] = 'Order slip creation failed';
+                    }
+                }
+
+                $this->respondAsJson($result);
             } else {
                 $this->respondError(400, 'Bad id_order');
             }
