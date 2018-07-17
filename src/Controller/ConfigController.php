949c69cd6b1e2d6b3331168b51e28f64ecd8dec1
Kévin S <kevin.sonnet@bwagence.fr>
2018-07-17 12:24:56 +0200

    Update payload config

--- src/Controller/ConfigController.php
+++ src/Controller/ConfigController.php
@@ -3,6 +3,7 @@
 namespace Oyst\Controller;
 
 use Carrier;
+use Configuration;
 use Country;
 use Language;
 use OrderState;
@@ -24,8 +25,9 @@ class ConfigController extends AbstractOystController
         $carriers = Carrier::getCarriers($id_lang, true, false, false, null, Carrier::ALL_CARRIERS);
         foreach ($carriers as $carrier) {
             $results['carriers'][] = array(
-                'id_carrier_reference' => $carrier['id_reference'],
-                'name' => $carrier['name'],
+                'label' => $carrier['name'],
+                'reference' => $carrier['id_reference'],
+                'delivery_delay' => $carrier['delay'],
             );
         }
 
@@ -33,21 +35,29 @@ class ConfigController extends AbstractOystController
         $countries = Country::getCountries($id_lang, true);
         foreach ($countries as $country) {
             $results['countries'][] = array(
-                'id_country' => $country['id_country'],
                 'name' => $country['name'],
-                'iso_code' => $country['iso_code'],
+                'code' => $country['iso_code'],
             );
         }
 
         //Get status
         $order_states = OrderState::getOrderStates($id_lang);
         foreach ($order_states as $order_state) {
-            $results['order_states'][] = array(
-                'id_order_state' => $order_state['id_order_state'],
-                'name' => $order_state['name'],
+            $results['order_statuses'][] = array(
+                'label' => $order_state['name'],
+                'code' => $order_state['id_order_state'],
             );
         }
 
         $this->respondAsJson($results);
+    }
+
+    public function setScriptTagUrl($params)
+    {
+        if (Configuration::updateValue('OYST_SCRIPT_TAG_URL', $params['data']['url'])) {
+            $this->respondAsJson(array('success' => true));
+        } else {
+            $this->respondError(400, "Error on update");
+        }
     }
 }
