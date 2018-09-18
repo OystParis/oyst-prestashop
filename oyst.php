d735d30f4d49ddc8462c87b111b5cf641df5bb61
yann <yann.aleman@oyst.com>
2018-09-18 11:42:22 +0200

    oyst-php  7.2.0-beta4

--- oyst.php
+++ oyst.php
@@ -32,7 +32,7 @@ class Oyst extends FroggyPaymentModule
     public function __construct()
     {
         $this->name = 'oyst';
-        $this->version = 'v1.21.0-YARC02';
+        $this->version = '1.21.0-YARC03';
         $this->tab = 'payments_gateways';
 
         parent::__construct();
