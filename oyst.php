eafbad15d4b174221fe2242c6ffebc3a827e771f
Kévin <kevin.sonnet@bwagence.fr>
2019-01-03 14:08:29 +0100

    Changer version to 1.28.2

--- oyst.php
+++ oyst.php
@@ -32,7 +32,7 @@ class Oyst extends FroggyPaymentModule
     public function __construct()
     {
         $this->name = 'oyst';
-        $this->version = '1.28.1';
+        $this->version = '1.28.2';
         $this->tab = 'payments_gateways';
 
         parent::__construct();
