2e933cbb11b4c9a057615d2f5fa05b32fb4ade0e
Kévin <kevin.sonnet@bwagence.fr>
2019-01-16 14:46:47 +0100

    Change version to RC28

--- oyst.php
+++ oyst.php
@@ -32,7 +32,7 @@ class Oyst extends PaymentModule
     public function __construct()
     {
         $this->name = 'oyst';
-        $this->version = '2.0.0';
+        $this->version = '2.0.0-RC28';
         $this->tab = 'payments_gateways';
 
         parent::__construct();
