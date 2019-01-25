88379b34e7291db988b460600fb3b3e0a7a831af
Kévin <kevin.sonnet@bwagence.fr>
2019-01-25 14:35:15 +0100

    Fix wording bug for product type + change version to 2.0.0-RC33

--- oyst.php
+++ oyst.php
@@ -32,7 +32,7 @@ class Oyst extends PaymentModule
     public function __construct()
     {
         $this->name = 'oyst';
-        $this->version = '2.0.0-RC31';
+        $this->version = '2.0.0-RC33';
         $this->tab = 'payments_gateways';
 
         parent::__construct();
