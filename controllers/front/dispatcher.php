d9aee02a1a233c0bf1f5ecacdfef5d4f1bba4d16
Kévin <kevin.sonnet@bwagence.fr>
2019-02-12 13:38:22 +0100

    Switch ws_key parameter and PHP_AUTH_USER

--- controllers/front/dispatcher.php
+++ controllers/front/dispatcher.php
@@ -52,10 +52,10 @@ class OystDispatcherModuleFrontController extends ModuleFrontController
                 $_SERVER['PHP_AUTH_USER'] = $matches[1];
             }
 
-            if (isset($_SERVER['PHP_AUTH_USER'])) {
-                $key = $_SERVER['PHP_AUTH_USER'];
-            } elseif (isset($_GET['ws_key'])) {
+            if (isset($_GET['ws_key'])) {
                 $key = $_GET['ws_key'];
+            } elseif (isset($_SERVER['PHP_AUTH_USER'])) {
+                $key = $_SERVER['PHP_AUTH_USER'];
             } else {
                 header($_SERVER['SERVER_PROTOCOL'].' 401 Unauthorized');
                 header('WWW-Authenticate: Basic realm="Welcome to Oyst Webservice, please enter the authentication key as the login. No password required."');
