f238b58d972e8e8323682d2e9b923a4c0e3b0bcb
Kévin <kevin.sonnet@bwagence.fr>
2019-03-19 14:46:35 +0100

    Change version to 2.1.12

--- oyst.php
+++ oyst.php
@@ -13,7 +13,7 @@ class Oyst extends PaymentModule
     public function __construct()
     {
         $this->name = 'oyst';
-        $this->version = '2.1.11';
+        $this->version = '2.1.12';
         $this->tab = 'payments_gateways';
 
         parent::__construct();
