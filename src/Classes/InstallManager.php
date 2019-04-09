82c1e31392c647263672967a01fcce6164c7e637
Kévin <kevin.sonnet@bwagence.fr>
2019-04-09 14:10:06 +0200

    Remove force attribute for API key generation

--- src/Classes/InstallManager.php
+++ src/Classes/InstallManager.php
@@ -76,7 +76,7 @@ class InstallManager
 		foreach ($shops as $shop_id) {
 			$shop_group_id = (int)Shop::getGroupFromShop($shop_id, true);
 
-			$state &= OystAPIKey::getShopInstance($shop_group_id, $shop_id)->generateAPIKey(true);
+			$state &= OystAPIKey::getShopInstance($shop_group_id, $shop_id)->generateAPIKey();
 			$state &= Configuration::updateValue('OYST_HIDE_ERRORS', 1, false, $shop_group_id, $shop_id);
 			$state &= Configuration::updateValue('OYST_ORDER_CREATION_STATUS', Configuration::get('OYST_OS_PAYMENT_CAPTURED'), false, $shop_group_id, $shop_id);
 		}
