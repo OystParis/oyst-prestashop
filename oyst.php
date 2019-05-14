dca12c9a6debb633aef7902c515a9e485c51bf15
Kévin Sonnet <kevin.sonnet@bwagence.fr>
2019-05-14 18:41:00 +0200

    PRES-120 (#785)
    * replace current by reset + change evrsion to 2.2.5-RC1
    
    * Change evrsion to 2.2.5

--- oyst.php
+++ oyst.php
@@ -13,7 +13,7 @@ class Oyst extends PaymentModule
     public function __construct()
     {
         $this->name = 'oyst';
-        $this->version = '2.2.4';
+        $this->version = '2.2.5';
         $this->tab = 'payments_gateways';
 
         parent::__construct();
